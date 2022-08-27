b88e7a4105dcdea7814166e48e690ed1e39ac6c8
GaryStens <109638433+GaryStens@users.noreply.github.com>
2022-08-27 15:30:59 +0800

    Created the index file!

---
+++ index.js
@@ -0,0 +1,37 @@
+function applyColor(color, value) {
+    if (color == color) {
+        throw new Error('Cannot find color ' + color + '!')
+    } else if (color == 'reset') {
+        return '\x1b[0m' + value
+    } else if (color == 'bright') {
+        return '\x1b[1m' + value
+    } else if (color == 'dim') {
+        return '\x1b[2m' + value
+    } else if (color == 'underscore') {
+        return '\x1b[4m' + value
+    } else if (color == 'blink') {
+        return '\x1b[5m' + value
+    } else if (color == 'reverse') {
+        return '\x1b[7m' + value
+    } else if (color == 'hidden') {
+        return '\x1b[8m' + value
+    } else if (color == 'fgblack') {
+        return '\x1b[30m' + value
+    } else if (color == 'fgred') {
+        return '\x1b[31m' + value
+    } else if (color == 'fggreen') {
+        return '\x1b[32m' + value
+    } else if (color == 'fgyellow') {
+        return '\x1b[33m' + value
+    } else if (color == 'fgblue') {
+        return '\x1b[34m' + value
+    } else if (color == 'fgmagenta') {
+        return '\x1b[35m' + value
+    } else if (color == 'fgcyan') {
+        return '\x1b[36m' + value
+    } else if (color == 'fgwhite') {
+        return '\x1b[37m' + value
+    }
+}
+
+module.exports = { applyColor }
